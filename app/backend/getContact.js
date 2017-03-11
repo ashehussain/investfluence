70b211ae2e40982eba9f3bd0b6269878d41c2fba
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2017-03-11 13:37:07 -0500

    Added npm run dev to start local mongo service.

---
+++ app/backend/getContact.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//Accepts an express query with a ticker symbol, queries the DB for the parsed contact information.
+
+module.exports = (req, res)=>{
+  console.log(req.param.ticker);
+}
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const p = process.cwd() + "/app/public";
+const p = process.cwd() + "/app/public",
+      getContact = require("../backend/getContact.js");
 
 module.exports = (app)=>{
 
   app.get('/', (req, res)=>res.sendFile(p + "/index.html"));
+  app.get('/contact/:ticker', getContact);
 
 }
