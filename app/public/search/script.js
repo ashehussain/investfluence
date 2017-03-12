12fb79c3beee99524bdf71278306bb3b18c239ae
Connie Cheng <connie.cheng52@gmail.com>
2017-03-11 23:38:54 -0500

    revised request to append instead of hiding data

--- app/public/search/script.js
+++ app/public/search/script.js
@@ -1,25 +1,71 @@
 $(document).ready(function(){
+ var getData = function(nameSearch){
+    return $.ajax({
+      url: 'http://localhost:8080/campaigns',
+      method: 'GET',
+      success: function(data){
+        console.log(data);
+        console.log(data[0])
+        handleResponse(data);
+      }
+    })
+  }
+  //search function to find character info;
+  var addAJAXFunction = function(data){
+    $('input').bind("enterKey",function(e){
+    // alert("Enter");
+      $('#campaignDisplay').show();
+        // e.preventDefault()
+      });
+      $('input').keydown(function(e){
+        if(e.keyCode == 13){
+          e.preventDefault()
+          $(this).trigger("enterKey");
+          getData()
+      }
 
-var availableTags = [
-      "Vanguard",
-      "something",
-      "something else",
-      "hi"
-    ];
-    $( "#tags" ).autocomplete({
-      source: availableTags
     });
-$('input').bind("enterKey",function(e){
-// alert("Enter");
-$('#campaignDisplay').show();
-e.preventDefault()
-});
-$('input').keyup(function(e){
-if(e.keyCode == 13)
-{
-  $(this).trigger("enterKey");
-}
-});
+
+    // $('#submitButton').click(function(event){
+    //   event.preventDefault();
+    //   var $name = $('input').val();
+    //   getData($name);
+    //   console.log($name);
+    //   $('.inputBox').addClass('hide');
+    // })
+  }
+  //changes text to display character data and picture
+  var handleResponse = function(data){
+    console.log(data[0])
+    //clear information on load
+    $('#information').html(" ")
+    $('#des').html(" ")
+    //on enter display information
+    var placeDes = "Pneumonia is one of te leading cases of death among children around the world. You can help make the cost of the vaccine affordable."
+    var $vals = $("<h4>Associated Campaigns</h4>"+
+      "<img src=''>"+
+      "<p>Description: " +placeDes+ "</p>" +
+      "<p># of Participants: " + 40 + "</p>" +
+      "<div class='progress'><div class='progress-bar progress-bar-success progress-bar-striped active' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='90' style='width:40%'>40%</div></div>" +
+      "<p class='expire'>Expires:" + data.expire + " </p>")
+    $('#campaignDisplay').append($vals)
+  };
+
+
+  //call search here
+  addAJAXFunction()
+
+  var availableTags = [
+        "Vanguard",
+        "something",
+        "something else",
+        "hi"];
+  $( "#tags" ).autocomplete({
+    source: availableTags
+  });
+
+
+
 })
 
 
