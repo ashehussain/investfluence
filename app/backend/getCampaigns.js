3ec78629dd83e5cfed7bdfcdf7d01b2b3ae1b5b5
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2017-03-11 23:39:02 -0500

    /campaigns works?

--- app/backend/getCampaigns.js
+++ app/backend/getCampaigns.js
@@ -1,36 +1,50 @@
 'use strict';
 
-module.exports = (req, res)=>{
-  console.log(req.query.ticker);
-  let dummy =  [{
-	"name": "T. Rowe Price International Gr & Inc",
-	"assets": {
-		"assetAllocation": [{
-			"ticker": "GSK",
-			"name": "GlaxoSmithKline",
-			"shares": 5566772,
-			"percentage_of_all_shares": 0.23,
-			"change_num_shares": 904000,
-			"change_percentage": 19.39,
-			"percent_total_assets": 1.82,
-			"mfund_ticker": "TRIGX"
-		}]
-	}
-}, {
-	"name": "Fidelity® Series Growth & Income",
-	"assets": {
-		"assetAllocation": [{
-			"ticker": "GSK",
-			"name": "GlaxoSmithKline",
-			"shares": 3163300,
-			"percentage_of_all_shares": 0.13,
-			"change_num_shares": 2100,
-			"change_percentage": 0.07,
-			"percent_total_assets": 1.37,
-			"mfund_ticker": "FGLGX"
-		}]
-	}
-}];
+const mongoose = require("mongoose");
 
-  res.send(JSON.stringify(dummy));
+module.exports = (req, res)=>{
+    let arr2 = [];
+    let arr = req.query.ticker.split(",");
+    let gotFundsObj = new Promise((resolve, reject)=>{
+        mongoose.connection.db.collection("Funds", (err, coll)=>{
+            if(err) reject(err);
+            coll.find({"ticker": {$in: arr}}).toArray((err2, doc)=>{
+                if(err2) reject(err2);
+                resolve(doc);
+            });
+        });
+    });
+
+    let promArr = [];
+
+    gotFundsObj.then((fundsObj)=>{
+        console.log("getfundsthen");
+        let assetAllocationsArr = fundsObj.forEach((fund)=>{
+            fund.assets.assetAllocation.forEach((stock)=>{
+                promArr.push(new Promise((resolve, reject)=>{
+                        mongoose.connection.db.collection("Campaigns", (err, coll)=>{
+                            if(err) throw err;
+                            console.log(stock.ticker);
+                            coll.find({"targetedStocks": {$elemMatch: {$eq: stock.ticker}}}).toArray((err2, doc)=>{
+                                if(err2) reject(err2);
+                                console.log(`Pushing arr2: ${JSON.stringify(doc)}`);
+                                arr2 = arr2.concat(doc);
+                                console.log(`Arr2: ${JSON.stringify(arr2)}`)
+                                resolve();
+                            });
+                        });
+                    })
+                )
+            });
+        })
+
+    console.log(promArr);
+
+    Promise.all(promArr).then((results)=>{
+        console.log("All promises done");
+        console.log(arr2);
+        res.send(arr2);
+    }, (err)=>console.log(err));
+    });
+
 }
